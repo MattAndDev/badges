e0630d5d9de82ea1287df5dd453813c6e78875f2
Matteo Maronati <me@mattinthe.net>
2021-05-14 16:49:18 +0200

    Fallback if no measure is avail

--- api/routes/shield/get-width.ts
+++ api/routes/shield/get-width.ts
@@ -8,12 +8,8 @@ export const getWidth = (
 ) => {
   return s.split('').reduce((acc, char, index) => {
     const utf = char.charCodeAt(0)
-    // TODO: emojis have an extra space for an unknown encoding reason
-    // find out why and fix properly
-    if (utf >= 50000 && index % 2 === 0) return acc
-
-    // @ts-ignore
-    const width = fontMap[fontId][char.charCodeAt(0)][fontSize]
+    const utfFallback = '0'.charCodeAt(0)
+    const width = fontMap[fontId][utf] ? fontMap[fontId][utf][fontSize] : fontMap[fontId][utfFallback][fontSize]
 
     return acc + width
   }, 0)
